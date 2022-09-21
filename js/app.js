858cb62028b468525dd69e3b374dc7be608d2451
joaquinneis <joaquinneis@gmail.com>
2022-09-21 20:24:54 -0300

    Carrito con localStorage

--- js/app.js
+++ js/app.js
@@ -1,28 +1,93 @@
-//Mostrar Productos del Arreglo en stock.js
-let mostrarProductos = (productos) => {
-    let contenedorProductos = document.getElementById("producto-contenedor")
+const contenedorProductos = document.getElementById("contenedor-productos");
 
-    for (let producto of productos) {
-        let div = document.createElement("div");
-        div.innerHTML = `<div class="card mx-auto" style="width: 18rem;">
-                                <img id="imagenProducto" src="${producto.imagen}" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 id="nombreProducto" class="card-title">${producto.nombre}</h5>
-                                    <h6 id="precioProducto" class="card-text">$${producto.precio}</h6>
-                                </div>
-                                <div class="card-body">
-                                    <button type="button" id="boton${producto.id}" class="btn btn-primary">Agregar al Carrito</button>
-                                </div>
-                            </div>`;
-        contenedorProductos.appendChild(div)
+const contenedorCarrito = document.getElementById("carrito-contenedor");
 
-        const boton = document.getElementById(`boton${producto.id}`)
+const botonVaciar = document.getElementById("vaciar-carrito");
 
-        boton.addEventListener(`click`, () => {
-            carritoIndex(producto.id)
-            alert(`Se agrego el producto ${producto.nombre}`)
-        })
+const contadorCarrito = document.getElementById("contadorCarrito");
+
+const precioTotal = document.getElementById("precioTotal");
+
+let carrito = [];
+
+//Cargar carrito desde el localStorage
+document.addEventListener("DOMContentLoaded", () =>{
+    if(localStorage.getItem("carrito")){
+        carrito = JSON.parse(localStorage.getItem("carrito"));
+        actualizarCarrito();
     }
+})
+
+//Vaciar Carrito
+botonVaciar.addEventListener("click", () => {
+    carrito.length = 0;
+    actualizarCarrito();
+})
+
+//Visualizar Cards de Productos
+for (let producto of stockProductos) {
+    let div = document.createElement("div");
+    div.innerHTML = `<div class="card mx-auto" style="width: 18rem;">
+                            <img id="imagenProducto" src="${producto.imagen}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 id="nombreProducto" class="card-title">${producto.nombre}</h5>
+                                <h6 id="precioProducto" class="card-text">$${producto.precio}</h6>
+                            </div>
+                            <div class="card-body">
+                                <button type="button" id="agregar${producto.id}" class="boton-agregar btn btn-primary">Agregar al Carrito</button>
+                            </div>
+                        </div>`;
+    
+                        contenedorProductos.appendChild(div);
+
+                        const boton = document.getElementById(`agregar${producto.id}`)
+
+                        boton.addEventListener("click", () => {
+                            agregarAlCarrito(producto.id);
+                        })
 }
 
-mostrarProductos(productos);
+//Agregar al carrito y Cantidades
+const agregarAlCarrito = (prodId) => {
+    const existe = carrito.some (prod => prod.id === prodId);
+
+    if (existe){
+        const prod = carrito.map (prod => {
+            if (prod.id === prodId){
+                prod.cantidad++;
+            }
+        })
+    } else {
+        const item = stockProductos.find ((prod) => prod.id === prodId);
+        carrito.push(item);
+    }
+    actualizarCarrito();
+}
+
+//Boton Eliminar
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId);
+    const indice = carrito.indexOf(item);
+    carrito.splice(indice, 1);
+    actualizarCarrito();
+}
+
+//Visualizar cada producto del Carrito
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = "";
+    carrito.forEach((prod) => {
+        const div = document.createElement("div");
+        div.className = ("productoEnCarrito");
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio: ${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick = "eliminarDelCarrito(${prod.id})" class="boton-eliminar"><img src="./assets/img/trash.svg" class="rounded float-end"></button>
+        `
+        contenedorCarrito.appendChild(div);
+
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    })
+    contadorCarrito.innerText = carrito.length;
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0);
+}
--- js/stock.js
+++ js/stock.js
@@ -1,14 +1,42 @@
-//Declaracion e Inicializacion de Variables
-let faciales;
-let cuerpo;
-let listado = [];
-let nombre = "";
-let producto = "";
-
-//Arreglo de Productos
-let productos = [
-    { id: 1, cod: 10058, nombre: "Leche Limpiadora Suavizante", imagen: "../assets/img/products/10058.webp", precio: 1860, categoria: faciales, cantidad: 2 },
-    { id: 2, cod: 10072, nombre: "Gel Limpiador Purificante", imagen: "../assets/img/products/10072.webp", precio: 1850, categoria: faciales, cantidad: 2 },
-    { id: 3, cod: 10065, nombre: "Locion Tonica Calmante", imagen: "../assets/img/products/10065.webp", precio: 1730, categoria: faciales, cantidad: 2 },
-    { id: 4, cod: 13868, nombre: "Agua Micelar Bifasica", imagen: "../assets/img/products/13868.webp", precio: 1860, categoria: cuerpo, cantidad: 2 },
+let stockProductos = [
+    { 
+        id: 1, 
+        cod: 10058, 
+        nombre: "Leche Limpiadora Suavizante", 
+        descripcion: "Elimina maquillaje e impurezas, manteniendo el equilibrio de hidratacion de la piel. Pieles normales, mixtas o secas. Apta hasta para las pieles mas sensibles.", 
+        imagen: "../assets/img/products/10058.webp", 
+        precio: 1860, 
+        categoria: "faciales", 
+        cantidad: 2 
+    },
+    { 
+        id: 2, 
+        cod: 10072, 
+        nombre: "Gel Limpiador Purificante", 
+        descripcion: "Permite una limpieza efectiva sin generar irritacion cutanea y sin dañar su equilibrio natural. Pieles normales, mixtas u oleosas. El enjuague debe ser completo sin dejar residuos.", 
+        imagen: "../assets/img/products/10072.webp", 
+        precio: 1850, 
+        categoria: "faciales", 
+        cantidad: 2 
+    },
+    { 
+        id: 3, 
+        cod: 10065, 
+        nombre: "Locion Tonica Calmante", 
+        descripcion: "Aporta minerales de propiedades desensibilizantes, que contribuyen a calmar y suavizar las pieles mas sensibles y reactivas. Rica en agua termal. Normaliza el pH luego de la limpieza. Muy indicado en pieles sensibles, sensibilizadas o intolerantes a los cosmeticos.", 
+        imagen: "../assets/img/products/10065.webp", 
+        precio: 1730, 
+        categoria: "faciales", 
+        cantidad: 2 
+    },
+    { 
+        id: 4, 
+        cod: 13868, 
+        nombre: "Agua Micelar Bifasica", 
+        descripcion: "Remueve hasta el maquillaje mas resistente, eliminando impurezas de la superficie de la piel sin dañar su equilibrio natural. Piel fresca y sin residuo oleoso.", 
+        imagen: "../assets/img/products/13868.webp", 
+        precio: 1860, 
+        categoria: "faciales", 
+        cantidad: 2 
+    },
 ];
